64a3f1f5de0d8ad122aa845cba29735484f39b16
kssanabria <kariestef20041998@gmail.com>
2024-02-16 12:18:17 -0500

    Se incluyo la serie fibonacci

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
  //parseFloat(prompt("Ingrese un número para calcular el Factorial"))
 
 
-function calcularFactorial(){
+function calcularFibonacci(){
     let number = document.getElementById("number").value;
     number=parseInt(number)
-    let factorial = 1;
+    let fibonacci= [0,1];
     if(number==null || /\D/.test(number) || number== ""){
         Swal.fire({
             title: `Hola, ${number} no es un número, ingresa un número válido`,
             icon: "error",
         })
     }else{
-    for(i=1; i<=number; i++){
-        factorial *= i;
+    for(i=2; i<number; i++){
+        fibonacci[i] = fibonacci [i-1] + fibonacci[i-2];
     }
     Swal.fire({
-        title: `El factorial de ${number} es ${factorial}`,
+        title: `La serie Fibonacci de ${number} es ${fibonacci}`,
         icon: "info",
     })
 }
